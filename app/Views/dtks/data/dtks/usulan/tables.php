d01b41aed44c082bb30e52775c83b558b34090c1
rsoeta <riansutarsa@outlook.com>
2022-11-21 05:58:54 +0700

    hidden tombol tambah data

--- app/Views/dtks/data/dtks/usulan/tables.php
+++ app/Views/dtks/data/dtks/usulan/tables.php
@@ -74,7 +74,7 @@
                                                         <i class="fa fa-sync-alt"></i> Reload
                                                     </button>
                                                 </div>
-                                                <div class="col-6 col-sm-3 mb-2">
+                                                <div class="col-6 col-sm-3 mb-2" hidden>
                                                     <button type="button" class="btn btn-primary btn-block tombolTambah">
                                                         <i class="fa fa-plus"></i> Tambah Data
                                                     </button>
