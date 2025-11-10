a2e94497707fd68f4626582a27b731a4a5e5ea33
Rian Sutarsa <riansoetarsa@outlook.com>
2025-11-10 20:20:39 +0700

    final: full Kint config stable for CI4.5.8 deployment

--- app/Config/Kint.php
+++ app/Config/Kint.php
@@ -2,62 +2,50 @@
 
 namespace Config;
 
-use Kint\Parser\ConstructablePluginInterface;
-use Kint\Renderer\Rich\TabPluginInterface;
-use Kint\Renderer\Rich\ValuePluginInterface;
+use Kint\Renderer\RichRenderer;
+use Kint\Renderer\TextRenderer;
 
-/**
- * --------------------------------------------------------------------------
- * Kint
- * --------------------------------------------------------------------------
- *
- * We use Kint's `RichRenderer` and `CLIRenderer`. This area contains options
- * that you can set to customize how Kint works for you.
- *
- * @see https://kint-php.github.io/kint/ for details on these settings.
- */
-class Kint
+class Kint extends \CodeIgniter\Config\BaseConfig
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Global Settings
-    |--------------------------------------------------------------------------
-    */
+    /** Maksimum kedalaman dump */
+    public $maxDepth = 6;
 
-    /**
-     * @var list<class-string<ConstructablePluginInterface>|ConstructablePluginInterface>|null
-     */
-    public $plugins;
+    /** Tampilkan file pemanggil */
+    public $displayCalledFrom = true;
 
-    public int $maxDepth           = 6;
-    public bool $displayCalledFrom = true;
-    public bool $expanded          = false;
+    /** Perluas struktur dump secara default */
+    public $expanded = false;
 
-    /*
-    |--------------------------------------------------------------------------
-    | RichRenderer Settings
-    |--------------------------------------------------------------------------
-    */
-    public string $richTheme = 'aante-light.css';
-    public bool $richFolder  = false;
+    /** Tema untuk RichRenderer */
+    public $richTheme = 'aante-light.css';
 
-    /**
-     * @var array<string, class-string<ValuePluginInterface>>|null
-     */
-    public $richObjectPlugins;
+    /** Folder output untuk RichRenderer */
+    public $richFolder = false;
 
-    /**
-     * @var array<string, class-string<TabPluginInterface>>|null
-     */
-    public $richTabPlugins;
+    /** Urutkan hasil RichRenderer */
+    public $richSort = true;
 
-    /*
-    |--------------------------------------------------------------------------
-    | CLI Settings
-    |--------------------------------------------------------------------------
-    */
-    public bool $cliColors      = true;
-    public bool $cliForceUTF8   = false;
-    public bool $cliDetectWidth = true;
-    public int $cliMinWidth     = 40;
+    /** Aktifkan warna pada output CLI */
+    public $cliColors = true;
+
+    /** Paksa output CLI menggunakan UTF-8 */
+    public $cliForceUTF8 = true;
+
+    /** Deteksi lebar terminal CLI */
+    public $cliDetectWidth = true;
+
+    /** Lebar minimum kolom CLI */
+    public $cliMinWidth = 40;
+
+    public function __construct()
+    {
+        // Setup untuk RichRenderer (browser)
+        RichRenderer::$folder = $this->richFolder;
+        RichRenderer::$theme  = $this->richTheme;
+
+        // Setup untuk TextRenderer (CLI)
+        if (property_exists(TextRenderer::class, 'decorations')) {
+            TextRenderer::$decorations = false;
+        }
+    }
 }
